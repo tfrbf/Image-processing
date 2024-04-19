79fa2e0b765f6cc2ab6a59ff505540623068024f
taha <amirtahafarshbaf@gmail.com>
2024-04-19 12:35:02 +0330

    test

--- Codes/training.py
+++ Codes/training.py
@@ -1,13 +1,8 @@
 import cv2 as cv
-import glob
 
-filepath = glob.glob("*.jpg")
+img = cv.imread("Images/tess.jpg")
 
-print("Your images: ", filepath)
+cv.imshow("tessssssss" , img )
 
-for image_path in filepath:
-    img = cv.imread(image_path)
-    cv.imshow("Your image with glob:", img)
-    cv.waitKey(1000)  # Wait until a key is pressed
-
+cv.waitKey(0)
 cv.destroyAllWindows()
