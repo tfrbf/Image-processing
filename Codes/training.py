11d6ec1ec0d48b805a7894f88d368a8e56751f67
taha <amirtahafarshbaf@gmail.com>
2024-04-19 12:26:36 +0330

    test

--- Codes/training.py
+++ Codes/training.py
@@ -1,8 +1,17 @@
 import cv2 as cv
 
 img = cv.imread("Images/tess.jpg")
+<<<<<<< HEAD
 
 cv.imshow("tessssssss" , img )
 
 cv.waitKey(0)
 cv.destroyAllWindows()
+=======
+
+
+cv.imshow("tessssssssss", img)
+
+cv.waitKey()
+cv.destroyAllWindows()
+>>>>>>> daf072d (test)
